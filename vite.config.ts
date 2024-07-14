f7e36b30b485af4640b0ae2cf48654e3c7b283b7
rose-charlotte <charlotte@anros.fr>
2024-07-14 18:16:37 +0200

    Re-add Vite as a bundler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import tsConfigPaths from "vite-tsconfig-paths";
+import * as packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        tsConfigPaths(),
+        dts({
+            include: ["src/"],
+        }),
+    ],
+    build: {
+        lib: {
+            entry: resolve("src", "index.ts"),
+            name: "@rose-charlotte/modal",
+            formats: ["es"],
+        },
+        rollupOptions: {
+            external: [...Object.keys(packageJson.peerDependencies)],
+        },
+    },
+});
